8cb1d69487225c99e9faf3734919ce3b00566a28
Fatemeh Brizendine <fatemehbrizendine@Fatemehs-MacBook-Pro.local>
2021-10-12 22:09:54 -0700

    added while

--- assets/js/game.js
+++ assets/js/game.js
@@ -20,9 +20,8 @@ console.log(enemyNames[3]);
 
 // fight function
 var fight = function(enemyName) {
-    // Alert players that they are starting the round
-    window.alert("Welcome to Robot Gladiators!");
-
+    // repeat and exeute as long as the enemy-robot is alive
+    while(enemyHealth > 0) {
     // ask player if they'd like to fight or run
     var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
     
@@ -77,9 +76,13 @@ var fight = function(enemyName) {
     }   else {
         window.alert("You need to choose a valid option. Try again!");
     }
+    }
+    
 }
 
 for(var i = 0; i < enemyNames.length; i++) {
-    fight(enemyNames[i]);
+    var pickedEnemyName = enemyNames[i];
+    enemyHealth = 50;
+    fight(pickedEnemyName);
 }
 
