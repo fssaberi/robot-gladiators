fe3e3814df71f162a729e4ad781c1cf6b85659b6
Fatemeh Brizendine <fatemehbrizendine@Fatemehs-MacBook-Pro.local>
2021-10-17 15:39:20 -0700

    fight/skip response

--- assets/js/game.js
+++ assets/js/game.js
@@ -11,15 +11,20 @@ var randomNumber = function(min, max) {
     return value;
 }
 
-// fight function
-var fight = function(enemy) {
-    // repeat and execute as long as the enemy-robot is alive
-    while (playerHealth > 0 && enemy.health > 0) {
-    // ask player if they'd like to fight or run
+var fightOrSkip = function () {
+    // ask player if they'd like to fight or skip
     var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
     
+    // Conditional Recursive Function Call
+    if (promptFight === "" || promptFight === null) {
+        window.alert("You need to provide a valid answer! Please try again.");
+        return fightOrSkip();
+    }
+
     // if player chooses to "skip" confirm and then stop the loop
-    if (promptFight === "skip" || promptFight === "SKIP") {
+    promptFight = promptFight.toLowerCase();
+
+    if (promptFight === "skip") {
     // confirm player wants to skip
         var confirmSkip = window.confirm("Are you sure you'd like to quit?");
 
@@ -28,9 +33,25 @@ var fight = function(enemy) {
             window.alert(playerInfo.name + " has decided to skip this fight. Goodbye!");
             // subtract money from playerInfo.money for skipping
             playerInfo.money = Math.max(0, playerInfo.money - 10);
-            console.log("playerInfo.money", playerInfo.money);
+
+            // return true if player wants to leave
+            return true;
+        }
+    }
+}
+
+// fight function
+var fight = function(enemy) {
+    // repeat and execute as long as the enemy-robot is alive
+    while (playerHealth > 0 && enemy.health > 0) {
+        // ask player if they'd like to fight or skip using fightOrSkip function
+        if (fightOrSkip()) {
+            // if true, leave fight by breaking loop
             break;
         }
+
+        fightOrSkip(); // <-- Replace code with this function call
+        var damage = randomNumber(playerInfo.attack - 3, playerInfo.attack);
     }
 
     // generate random damage value based on player's attack power
